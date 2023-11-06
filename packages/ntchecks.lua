b02f01bc2c02079ec40cc152cc5b3d1106738419
nevertrollsh <143024241+nevertrollsh@users.noreply.github.com>
2023-11-06 17:37:48 +0530

    Create ntchecks.lua

---
+++ packages/ntchecks.lua
@@ -0,0 +1,13 @@
+nt = {}
+
+function nt.isNumber(x) 
+ return type(x) == "number"
+end
+
+function nt.isString(x)
+ return type(x) == "string"
+end
+
+function nt.isTable(x)
+  return type(x) == "table"
+end
