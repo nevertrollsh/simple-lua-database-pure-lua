44f12484bb32eeea0e970d656646a5352aea6821
nevertrollsh <143024241+nevertrollsh@users.noreply.github.com>
2023-11-06 17:36:57 +0530

    Create packages.lua

---
+++ packages.lua
@@ -0,0 +1,22 @@
+packages = {"ntdatabase","tables","random","ntchecks","ntgames"}
+
+function sleep(seconds)
+    os.execute("sleep " .. tonumber(seconds))
+end
+
+function loadPackage(pkg)
+dofile(pkg)
+end
+
+
+local database = loadPackage("packages/ntdatabase.lua");
+
+local tables = loadPackage("packages/table.lua")
+
+local random = loadPackage("packages/random.lua")
+
+local ntchecks = loadPackage("packages/ntchecks.lua")
+
+local ntgames = loadPackage("packages/ntgames.lua")
+
+
